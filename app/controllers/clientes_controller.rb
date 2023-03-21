ad5383da2213c438e97ea7098175c077c3535e2a
DiegoGaxi <diego.gaxi@hotmail.com>
2023-03-21 16:08:54 -0700

    changes

--- app/controllers/clientes_controller.rb
+++ app/controllers/clientes_controller.rb
@@ -3,13 +3,12 @@ class ClientesController < ApplicationController
   # Obtener el cliente simulado
   def getClienteMongo
 
-    # Se obtiene el cliente llamando a la base de datos MongoDB
-    @client = Cliente.where(id: 1).first
-    cliente = @client.attributes
-
+    # Se obtiene el cliente 
+    cliente = Cliente.new
+    cliente.name = 'Diego Gaxiola Development'
     # Se obtiene el prestamo del cliente llamando a la base de datos PostgreSQL
-    cliente[:loans] = Loan.where(cliente_id: 1)
-    cliente[:prestado] = cliente[:loans].sum(:principal)
+    cliente.loans = Loan.all
+    cliente.prestado = cliente.loans.sum(:principal)
     
     render json: cliente
   end
@@ -18,5 +17,4 @@ class ClientesController < ApplicationController
   def index
     @clientes = Cliente.all
   end
-
 end
--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,10 +1,11 @@
 class Cliente
+  attr_accessor :name, :loans, :prestado
   # MongoDB connection and attributes
-  include Mongoid::Document
-  include Mongoid::Timestamps
-  field :name, type: String
-  field :id, type: Integer
+#   include Mongoid::Document
+#   include Mongoid::Timestamps
+#   field :name, type: String
+#   field :id, type: Integer
 
-  validates_presence_of :name
+#   validates_presence_of :name
   # has_many :loans
 end
